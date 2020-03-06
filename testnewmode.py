2eeaec5b6ddd28d3578e8ac561032dee7c3da673
Kevin Porras <kporras07@gmail.com>
2020-03-06 08:06:31 -0600

    Update testnewmode file.

--- testnewmode.py
+++ testnewmode.py
@@ -1,5 +1,14 @@
 from newmode import Newmode
 
-api = Newmode(api_version='v1.0')
+api = Newmode()
 
-print(api.getTool(73))
+payload = {
+	"first_name": "Mark",
+	"last_name": "Styles",
+	"email_address": "lambic@pm.me",
+	"postal_code": "H4E 2Y7",
+	"email_subject": "This is my subject",
+	"your_letter": "This is my letter"
+}
+
+print(api.getOutreach(37))
