5bd1e5fbe57930111b47497534995e46286ea0b0
Kevin Porras <kporras07@gmail.com>
2020-03-09 14:53:52 -0600

    Bump version up

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="newmode",
-    version="0.1.1",
+    version="0.1.2",
     author="New/Mode",
     author_email="info@newmode.net",
     description="New/Mode API Wrapper",
